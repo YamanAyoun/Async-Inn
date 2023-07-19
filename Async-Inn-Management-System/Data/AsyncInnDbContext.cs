d5c13d97580328a6b50b8ad0689c3dfb4d57ff26
YamanAyoun <yamanayoun0@gmail.com>
2023-07-19 20:39:12 +0300

    Add project files.

---
+++ Async-Inn-Management-System/Data/AsyncInnDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Async_Inn_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Async_Inn_Management_System.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Hotel> hotels { get; set; }
+
+        public DbSet<Room> rooms { get; set; } 
+
+        public DbSet<Amenity> amenities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel { Id = 1, Name = "Hotel Amman", StreetAddress = "s-077", City = "Amman", State = "Amman", Country = "Jordan", Phone = 0784234 },
+                new Hotel { Id = 2, Name = "Hotel Aqaba", StreetAddress = "a-137", City = "Aqaba", State = "Aqaba", Country = "Jordan", Phone = 0742342 },
+                new Hotel { Id = 3, Name = "Hotel Dead Sea", StreetAddress = "c-237", City = "Dead Sea", State = "Deadsea", Country = "Jordan", Phone = 0785483 }
+            );
+
+
+            modelBuilder.Entity<Room>().HasData(
+                new Room { Id = 1, Name = "Stander Room", Layout = 1 },
+                new Room { Id = 2, Name = "Suite Room", Layout = 2 },
+                new Room { Id = 3, Name = "Executive suite Room", Layout = 3 }
+            );
+
+
+            modelBuilder.Entity<Amenity>().HasData(
+                new Amenity { Id = 1, Name = "Stander Amenity" },
+                new Amenity { Id = 2, Name = "Medium Amenity" },
+                new Amenity { Id = 3, Name = "Superior Amenity" }
+            );
+        }
+    }
+}
---
+++ Async-Inn-Management-System/Program.cs
@@ -0,0 +1,32 @@
+using Async_Inn_Management_System.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Async_Inn_Management_System
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            string connString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services
+                .AddDbContext<AsyncInnDbContext>
+                (opions => opions.UseSqlServer(connString));
+
+            
+
+            var app = builder.Build();
+
+            app.MapControllers();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
