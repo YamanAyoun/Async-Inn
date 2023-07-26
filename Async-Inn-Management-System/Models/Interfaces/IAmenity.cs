4fb6ad7978fcbed9f25f13630ddc9f2f074f3b80
YamanAyoun <yamanayoun0@gmail.com>
2023-07-26 10:27:43 +0300

    update

--- Async-Inn-Management-System/Models/Interfaces/IAmenity.cs
+++ Async-Inn-Management-System/Models/Interfaces/IAmenity.cs
@@ -2,5 +2,19 @@
 {
     public interface IAmenity
     {
+        // Create
+        Task<Amenity> Create(Amenity amenity);
+        
+        // GET All
+        Task<List<Amenity>> GetAmenities();
+        
+        // GET by ID
+        Task<Amenity> GetAmenity(int id);
+        
+        // UPDATE
+        Task<Amenity> UpdateAmenity(int id, Amenity amenity);
+        
+        // DELET by ID
+        Task Delete(int id);
     }
 }
--- Async-Inn-Management-System/Models/Services/RoomServices.cs
+++ Async-Inn-Management-System/Models/Services/RoomServices.cs
@@ -1,6 +1,45 @@
-﻿namespace Async_Inn_Management_System.Models.Services
+﻿using Async_Inn_Management_System.Data;
+using Async_Inn_Management_System.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Async_Inn_Management_System.Models.Services
 {
-    public class RoomServices
+    public class RoomServices : IRoom
     {
+        private readonly AsyncInnDbContext _context;
+
+        public RoomServices(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Room> Create(Room room)
+        {
+            _context.Entry(room).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+            return room;
+        }
+        public async Task<List<Room>> GetRooms()
+        {
+            var rooms = await _context.rooms.ToListAsync();
+            return rooms;
+        }
+        public async Task<Room> GetRoom(int id)
+        {
+            Room room = await _context.rooms.FindAsync(id);
+            return room;
+        }
+        public async Task<Room> UpdateRoom(int id, Room room)
+        {
+            _context.Entry(room).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return room;
+        }
+        public async Task Delete(int id)
+        {
+            Room room = await GetRoom(id);
+            _context.Entry(room).State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
     }
 }
